abf772c8607c178815175569893217c06fa624ce
Haxfx <sebastiaan@hotflux.nl>
2021-04-13 21:08:49 +0200

    feat(transition): added page transition on text with gsap ease, exit and enter with gsap and csstransition

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Title from '../components/Title';
+
+const Home = () => (
+  <div className="inner">
+    <Title lineContent="Solving Solutions that" lineContent2="need expert insight" />
+    <div>
+      <p className="other">
+        Does project report used question death, out more rhetoric unpleasing what compared both of
+        sentinels.
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
